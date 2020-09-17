3ff298af317fce1939d19506b68a25c84cb0e0e6
unknown <lucasmatoscastelo@gmail.com>
2020-09-17 14:45:29 -0400

    script basico do painel

---
+++ VisualShiny.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(shinydashboard)
+library(dashboardthemes)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    fluidRow( box(
+      title = "Controls",
+      sliderInput("slider", "Number of observations:", 1, 100, 50),
+      width = 4
+    ),
+      box(plotOutput("plot1", height = 250),width = 8)
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$plot1 <- renderPlot({
+    
+  })
+}
+
+shinyApp(ui, server)
